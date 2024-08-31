566487d43fa7142429559c6a6d5d3323b8059fea
ruizhipu <18651885620@163.com>
2024-08-31 11:09:19 +0800

    add

--- examples/text_to_image/infer_lora.py
+++ examples/text_to_image/infer_lora.py
@@ -14,6 +14,7 @@ pipe.unet.load_attn_procs(model_path)
 pipe.to("cuda")
 
 
+
 des = args.description
 prompt = "A woman with {}".format(des)
 image = pipe(prompt, num_inference_steps=30, guidance_scale=7.5).images[0]
